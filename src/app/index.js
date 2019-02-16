935b58142e4ade3be9e666315bdf9745b1e73b8a
Hosam <hossamnasser938@gmail.com>
2019-02-16 17:27:23 +0200

    Use Redux alone

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,39 @@
-console.log( "Working ISA" );
+import { createStore } from 'redux';
+
+const initialState = 0;
+
+const reducer = ( state, action ) => {
+    let copiedState = state;
+
+    switch( action.type ) {
+        case "ADD":
+            copiedState += action.payload;
+            break;
+        case "SUBTRACT":
+            copiedState -= action.payload;
+            break;
+    }
+
+    return copiedState;
+};
+
+const store = createStore( reducer, initialState );
+
+store.subscribe( () => {
+    console.log( "Updated state", store.getState() );
+} );
+
+store.dispatch( {
+    type: "ADD",
+    payload: 4
+} );
+
+store.dispatch( {
+    type: "ADD",
+    payload: 9
+} );
+
+store.dispatch( {
+    type: "SUBTRACT",
+    payload: 3
+} );
